111001930318c167655e33542fbee602118d5474
Nicole Cordes <cordes@cps-it.de>
2018-01-25 11:39:35 +0100

    Allow flexible fields for group and user admin module

--- ext_localconf.php
+++ ext_localconf.php
@@ -21,4 +21,22 @@ if (TYPO3_MODE) {
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Backend\\Controller\\ContentElement\\ElementInformationController'] = array(
         'className' => 'dkd\\TcBeuser\\Xclass\\RecordInfoController'
     );
+
+    //add formdataprovider to enable exclude of be_users fields
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['formDataGroup']['tcaDatabaseRecord'][\dkd\TcBeuser\Form\FormDataProvider\BackendExcludeTableEnable::class] = [
+        'depends' => [
+            \TYPO3\CMS\Backend\Form\FormDataProvider\TcaCheckboxItems::class,
+        ],
+        'before' => [
+            \TYPO3\CMS\Backend\Form\FormDataProvider\TcaSelectItems::class,
+        ]
+    ];
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['formDataGroup']['tcaDatabaseRecord'][\dkd\TcBeuser\Form\FormDataProvider\BackendExcludeTableDisable::class] = [
+        'depends' => [
+            \TYPO3\CMS\Backend\Form\FormDataProvider\TcaSelectItems::class,
+        ],
+        'before' => [
+            \TYPO3\CMS\Backend\Form\FormDataProvider\TcaSelectTreeItems::class,
+        ]
+    ];
 }
